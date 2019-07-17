95d5348d237ce3920b070c83bb79a2ceb5d8357d
samipkalyani <samipkalyani004@gmail.com>
2019-07-17 16:20:38 +0530

    login_queries

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,10 +1,19 @@
 const db = require('../utils/database.js');
 
+// exports.mainpage = async (req, res) => {
+//   try{
+//   const result = await db.execute('SELECT * FROM teachers');
+//   console.log(result);
+//   }catch(err){
+//     console.log(err);
+//   }
+// };
+
 exports.mainpage = async (req, res) => {
-  try{
-  const result = await db.execute('SELECT * FROM teachers');
-  console.log(result);
-  }catch(err){
-    console.log(err);
-  }
+  const type = req.query.type
+  res.status(422).render('index', {
+    path: '/dashboard',
+    pageTitle: 'Mentorship_Forum',
+    type
+  });
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const main = require("../controllers/main");
+const main = require("../controllers/main.js");
+const auth = require("../controllers/auth.js")
 
-router.get("/",main.mainpage);
+router.get("/dashboard",main.mainpage);
+router.post("/studentlogin",auth.postStudentLogin);
+router.post("/teacherlogin",auth.postTeacherLogin);
 
 module.exports = router;

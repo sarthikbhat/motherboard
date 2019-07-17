10a373a5ff36336c73268e2a0c0a9db79dc0b08e
samipkalyani <samipkalyani004@gmail.com>
2019-07-17 14:21:22 +0530

    db&sql_scripts

--- server/app.js
+++ server/app.js
@@ -7,7 +7,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes)
 
--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,3 +1,10 @@
-exports.mainpage = (req, res, next) => {
-    res.send("Server Works");
-  };
+const db = require('../utils/database.js');
+
+exports.mainpage = async (req, res) => {
+  try{
+  const result = await db.execute('SELECT * FROM teachers');
+  console.log(result);
+  }catch(err){
+    console.log(err);
+  }
+};

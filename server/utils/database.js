8996c63eabe8e948291810bc757de278ebb1f667
SANJAY NAYAK <sanjayjnayak99@gmail.com>
2019-07-21 12:08:29 +0000

    Revert "Initial backend changes (pull request #2)"
    This reverts pull request #2.
    
    > Initial backend changes

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,23 +1,8 @@
-/**
- * MongoDB configuration and server connection using mongoose 
- */
-
-const mongoose = require('mongoose');
-const {MONGO_URL} = require('../config/config');
-
-const conn = mongoose.createConnection(MONGO_URL, {useNewUrlParser: true, useCreateIndex: true});
-
-conn.mongo = mongoose.mongo;
-
-//Error
-conn.on('error', function(err){
-  console.log('Error connecting to database');
-  console.log(err);
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'mentorship_forum',
+    password: ''
 });
-
-//Connected
-conn.on('connected', function(){
-  console.log('Connected to database');
-});
-
-exports.default = conn;
+module.exports = pool.promise();

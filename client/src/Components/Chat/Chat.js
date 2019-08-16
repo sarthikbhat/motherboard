c56f5be265f8a9afef4ff98b64ccbbcc2c08442f
Sanjay <sanjayjnayak99@gmail.com>
2019-08-16 22:17:11 +0530

    merge->sartik(art370) to sanjay(Satrick)

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -36,24 +36,24 @@ export default class Chat extends Component {
         return (
             <div id="chatBody" >
                 <div id="chatSect" >
-                    <ChatBar/>
+                    <ChatBar />
                 </div>
                 <div className={listStud} >
+                    <ContactBar />  
                     {
                         this.state.width <= 600 ? (
-                            <button 
-                            style={{ position: "fixed", background: "rgb(256,256,256)", left: -55, marginTop: 30, padding: 12, paddingRight: 16, paddingBottom: 10, borderRadius: 6 }}
-                            onClick={this.contactBarToggler} >
+                            <button
+                                style={{ position: "fixed", background: "rgb(256,256,256)", left: -55, marginTop: 30, padding: 12, paddingRight: 16, paddingBottom: 10, borderRadius: 6 }}
+                                onClick={this.contactBarToggler} >
                                 <Forum />
                             </button>
                         ) : (null)
                     }
                 </div>
-                {this.state.openContactBar===true?
-                    <div style={{position:"absolute",width:"100%",height:"100%",background:"rgba(0,0,0,0.7)",zIndex:100}} onClick={this.contactBarToggler}>
-                        <ContactBar/>
+                {this.state.openContactBar === true ?
+                    <div style={{ position: "absolute", width: "100%", height: "100%", background: "rgba(0,0,0,0.7)", zIndex: 100 }} onClick={this.contactBarToggler}>
                     </div>
-                    :null
+                    : null
                 }
             </div>
         )
---
+++ client/src/Components/Navigator/Navigator.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Navigator.scss'
+
+export default class Navigator extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                {/*bhatsartik28*/}
+            </React.Fragment>
+        )
+    }
+}

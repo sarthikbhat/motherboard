8376a4a43ea8307ac4c145be237d69c470c74edd
sarthikbhat <bhatsarthik28@gmail.com>
2019-08-18 02:08:26 +0530

    Navigator done and group section fix

--- client/src/Components/Navigator/Navigator.js
+++ client/src/Components/Navigator/Navigator.js
@@ -5,7 +5,19 @@ export default class Navigator extends Component {
     render() {
         return (
             <React.Fragment>
-                {/*bhatsartik28*/}
+            <div className="nav-header" >
+                <div className="navigators">
+                    <h3>CHATS</h3>
+                </div>
+            </div>
+            <div id="navigateOuterBox" style={{ height: window.innerHeight - 148 }} >
+                <div id="navigate">
+                    <div className="content"><h3>Girevances</h3></div>
+                    <div className="content"><h3>Community</h3></div>
+                    <div className="content"><h3>Notes</h3></div>
+                    <div className="content"><h3>Events/Calander</h3></div>
+                </div>
+            </div>
             </React.Fragment>
         )
     }

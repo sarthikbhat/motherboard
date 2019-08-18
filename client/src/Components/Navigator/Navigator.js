7689de0345fa26422c68e4d5bf97f756b43b9143
sarthikbhat <bhatsarthik28@gmail.com>
2019-08-18 22:04:33 +0530

    Color scheme updated

--- client/src/Components/Navigator/Navigator.js
+++ client/src/Components/Navigator/Navigator.js
@@ -5,13 +5,9 @@ export default class Navigator extends Component {
     render() {
         return (
             <React.Fragment>
-            <div className="nav-header" >
-                <div className="navigators">
-                    <h3>CHATS</h3>
-                </div>
-            </div>
-            <div id="navigateOuterBox" style={{ height: window.innerHeight - 148 }} >
+            <div id="navigateOuterBox" style={{ height: window.innerHeight - 69 }} >
                 <div id="navigate">
+                    <div className="content"><h3>Chats</h3></div>
                     <div className="content"><h3>Girevances</h3></div>
                     <div className="content"><h3>Community</h3></div>
                     <div className="content"><h3>Notes</h3></div>

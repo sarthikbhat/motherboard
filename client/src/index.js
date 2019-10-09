b7c2e7ca00ea53302b6184d0bb64813a82db722e
Sanjay <sanjayjnayak99@gmail.com>
2019-10-09 10:08:46 +0530

    apollo setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { IntlProvider } from "react-intl";
 import createHistory from "history/createBrowserHistory";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+
 import './index.css';
 import App from './App';
-
 import configureStore from "./reduxStates/store";
 
 const basename = process.env.REACT_APP_ROUTER_BASENAME || "";
 const history = createHistory({ basename });
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphQl"
+});
 
 const store = configureStore({}, history);
 
@@ -18,28 +26,32 @@ const rootElement = document.getElementById("root");
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
-    <IntlProvider locale="en">
+  <IntlProvider locale="en">
+    <ApolloProvider client={client}>
       <Provider store={store}>
         <App history={history} />
       </Provider>
-    </IntlProvider>,
-    rootElement
-  );
-  
-  if (module.hot) {
-    module.hot.accept("./App", () => {
-      // eslint-disable-next-line global-require
-      const NextApp = require("./App").default;
-      ReactDOM.render(
-        <IntlProvider locale="en">
+    </ApolloProvider>
+  </IntlProvider>,
+  rootElement
+);
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require("./App").default;
+    ReactDOM.render(
+      <IntlProvider locale="en">
+        <ApolloProvider client={client}>
           <Provider store={store}>
             <NextApp history={history} />
           </Provider>
-        </IntlProvider>,
-        rootElement
-      );
-    });
-  }
+        </ApolloProvider>
+      </IntlProvider>,
+      rootElement
+    );
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

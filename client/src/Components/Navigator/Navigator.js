cee2aa3e06ab744cb90df347abd55ea6001b4103
sarthikbhat <bhatsarthik28@gmail.com>
2019-10-23 14:17:11 +0530

    Icons added

--- client/src/Components/Navigator/Navigator.js
+++ client/src/Components/Navigator/Navigator.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import chats from '../../images/chat.png';
+import notes from '../../images/notes.png';
+import events from '../../images/events.png';
+import logout from '../../images/logout.png';
+import addAttend from '../../images/addAttend.png';
+import attend from '../../images/attend.png';
+import response from '../../images/response.png';
+import grief from '../../images/grief.png';
 import './Navigator.scss';
 
 export default class Navigator extends Component {
@@ -30,7 +37,7 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={grief} alt="" />
               </Grid>
               <Grid item xs={9}>
                 <Link to="/grievances" style={{ color: 'rgba(251, 246, 246,0.9)' }} id="grievances">
@@ -40,7 +47,7 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={response} alt="" />
               </Grid>
               <Grid item xs={9}>
                 {' '}
@@ -51,7 +58,7 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={notes} alt="" />
               </Grid>
               <Grid item xs={9}>
                 {' '}
@@ -62,7 +69,7 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={events} alt="" />
               </Grid>
               <Grid item xs={9}>
                 {' '}
@@ -73,7 +80,7 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={attend} alt="" />
               </Grid>
               <Grid item xs={9}>
                 <Link to="/attendance" style={{ color: 'rgba(251, 246, 246,0.9)' }} id="attendance">
@@ -83,7 +90,7 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={addAttend} alt="" />
               </Grid>
               <Grid item xs={9}>
                 <Link to="/attendanceadd" style={{ color: 'rgba(251, 246, 246,0.9)' }} id="attendance">
@@ -93,10 +100,10 @@ export default class Navigator extends Component {
             </Grid>
             <Grid container spacing={32} className="content">
               <Grid item xs={3} className="item">
-                <img className="ficon" src={chats} alt="" />
+                <img className="ficon" src={logout  } alt="" />
               </Grid>
               <Grid item xs={9}>
-                <div className="content" id="log-out" onClick={this.logout}>
+                <div style={{ color: 'rgba(251, 246, 246,0.9)' }} id="log-out" onClick={this.logout}>
                   <h3>Log Out</h3>
                 </div>
               </Grid>
